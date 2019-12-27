e3fe736507fdeba595ac2a630c841afd1649d4df
Gyutae Park <gtp7473@sk.com>
2019-12-27 17:49:55 +0900

    make parse function and output

---
+++ parse/go.mod
@@ -0,0 +1,5 @@
+module github.com/rbxorkt12/apposync/parse
+
+go 1.13
+
+require github.com/spf13/viper v1.6.1
--- parse/parse.go
+++ parse/parse.go
@@ -1,1 +1,79 @@
-unimplemented
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"os"
+)
+
+type Appoconfig struct {
+	Kind string
+	Images []Image
+}
+type Image struct {
+	Name string
+	Gitrepos []string
+}
+
+func removeDuplicates(elements []string) []string {
+	// Use map to record duplicates as we find them.
+	encountered := map[string]bool{}
+	result := []string{}
+
+	for _,v := range elements {
+		if encountered[v] == true {
+			// Do not add duplicate.
+		} else {
+			// Record this element as an encountered element.
+			encountered[v] = true
+			// Append to result slice.
+			result = append(result, v)
+		}
+	}
+	// Return the new slice.
+	return result
+}
+
+
+
+func main(){
+	appoconfig := &Appoconfig{}
+	viper.SetConfigName("Apposync")
+	viper.AddConfigPath("/tmp")
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Errorf("Can't read config file: %s \n", err)
+	}
+	if err := viper.Unmarshal(appoconfig); err != nil {
+		fmt.Errorf("config file format error: %s \n", err)
+	}
+	fo1, err := os.Create("./parse_images.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer fo1.Close()
+	fo2, err := os.Create("./parse_repoes.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer fo2.Close()
+	var reponames []string
+	for _,image := range appoconfig.Images{
+		_,err1:=fo1.WriteString(image.Name)
+		if err1 != nil {
+			panic(err)
+		}
+		for _,reponame := range image.Gitrepos{
+			reponames = append(reponames,reponame)
+
+		}
+		_,err1 = fo1.WriteString("\n")
+	}
+	reponames=removeDuplicates(reponames)
+	for _, reponame := range reponames{
+		_,err2 := fo2.WriteString(reponame)
+		if err2 != nil {
+			panic(err)
+		}
+		_,err2 = fo2.WriteString("\n")
+	}
+}
